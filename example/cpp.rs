20a71f33b9f338c8fab473b367c131836bf0b174
Ben Foppa <benjamin.foppa@gmail.com>
2015-05-04 20:42:42 -0400

    Moved Rust extern decls into a separate module.

---
+++ example/cpp.rs
@@ -0,0 +1,4 @@
+extern {
+  pub fn cpp_foo(_0: i32);
+  pub fn cpp_bar_int() -> i32;
+}
--- example/main.rs
+++ example/main.rs
@@ -1,19 +1,14 @@
-mod mycpp {
-  extern {
-    pub fn cpp_foo(_0: i32);
-    pub fn cpp_bar_int() -> i32;
-  }
-}
+mod cpp;
 
 fn main() {
   println!("Calling cpp_bar_int");
   let x = unsafe {
-    mycpp::cpp_bar_int()
+    cpp::cpp_bar_int()
   };
   println!("cpp_bar_int returned {}", x);
   println!("");
   println!("Calling cpp_foo");
   unsafe {
-    mycpp::cpp_foo(x);
+    cpp::cpp_foo(x);
   }
 }
