b31dc18b3886fd2699fb4b66553ff26c19e6e458
Ben Foppa <benjamin.foppa@gmail.com>
2015-05-23 11:26:11 -0400

    Nicer example output

--- example/main.rs
+++ example/main.rs
@@ -9,34 +9,34 @@ mod mycpplib {
 }
 
 fn main() {
-  cpp!();
-
-  println!("[rust] Calling cpp_bar_int");
+  println!("[rust] Calling bar<int>");
   let x = unsafe {
     mycpplib::cpp_bar_int()
   };
-  println!("[rust] cpp_bar_int returned {:?}", x);
+  println!("[rust] bar<int> returned {:?}", x);
   println!("");
-  println!("[rust] Calling cpp_foo");
+  println!("[rust] Calling foo");
   let x = unsafe {
     mycpplib::cpp_foo(x)
   };
-  println!("[rust] cpp_foo returned {:?}", x);
+  println!("[rust] foo returned {:?}", x);
   println!("");
-  println!("[rust] Calling cpp_Foo_int_new_int with 3");
+  println!("[rust] Calling new Foo<int> with 3");
   let p_foo = unsafe {
     mycpplib::cpp_Foo_int_new_int(3)
   };
+  println!("[rust] new Foo<int> returned {:?}", p_foo);
   println!("");
-  println!("[rust] Calling cpp_Foo_int_x_");
+  println!("[rust] Calling Foo<int>::x_");
   let x_ = unsafe {
     let p_x_ = mycpplib::cpp_Foo_int_x_(p_foo) as *const libc::c_int;
     *p_x_
   };
-  println!("[rust] cpp_Foo_x_ returned &{}", x_);
+  println!("[rust] Foo<int>::x_ returned &{}", x_);
   println!("");
-  println!("[rust] Calling cpp_Foo_int_delete");
-  unsafe {
-    mycpplib::cpp_Foo_int_delete(p_foo);
-  }
+  println!("[rust] Calling delete Foo<int>");
+  let x = unsafe {
+    mycpplib::cpp_Foo_int_delete(p_foo)
+  };
+  println!("[rust] Foo<int> returned {:?}", x);
 }
