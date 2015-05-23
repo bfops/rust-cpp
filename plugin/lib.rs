bde422a47dae6dda506d5134b41f97da5fee9b70
Ben Foppa <benjamin.foppa@gmail.com>
2015-05-23 11:20:53 -0400

    Fix typos/warnings

--- plugin/lib.rs
+++ plugin/lib.rs
@@ -5,33 +5,29 @@ extern crate rustc;
 
 use syntax::codemap::Span;
 use syntax::ast::TokenTree;
-use syntax::ext::base::{ExtCtxt, MacResult, MacEager};
+use syntax::ext::base::{ExtCtxt, MacResult};
 use syntax::ext::build::AstBuilder;
 use rustc::plugin::Registry;
 
-fn scape_cpp(cxt: &mut ExtCtxt, span: Span, _: &[TokenTree]) -> Box<MacResult + 'static> {
-  syntax::ext::base::DummyResult::any()
+fn scrape_cpp(cxt: &mut ExtCtxt, span: Span, _: &[TokenTree]) -> Box<MacResult + 'static> {
+  syntax::ext::base::DummyResult::any(span)
 }
 
-fn bind_cpp(cxt: &mut ExtCtxt, span: Span, _: &[TokenTree]) -> Box<MacResult + 'static> {
-  assert!(false);
+fn call_cpp(_: &mut ExtCtxt, _: Span, _: &[TokenTree]) -> Box<MacResult + 'static> {
+  panic!("call_cpp unimplemented");
 }
 
 #[plugin_registrar]
 pub fn plugin_registrar(reg: &mut Registry) {
-  let macro_binding;
   match std::env::var("PREBUILD") {
-    Ok(val) => {
-      macro_binding = scrape_cpp;
+    Ok(_) => {
+      reg.register_macro("cpp", scrape_cpp);
     }
     Err(std::env::VarError::NotPresent) => {
-      macro_binding = call_cpp;
+      reg.register_macro("cpp", call_cpp);
     },
     e => {
       e.unwrap();
-      return
     }
   }
-
-  reg.register_macro("cpp", macro_binding);
 }
